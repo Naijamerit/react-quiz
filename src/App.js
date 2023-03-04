fed3d5efe61d8824f521e4217918952e8142114e
Naijamerit <victor.ejiogu@ust.edu.ng>
2023-03-04 07:30:30 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import File from './File';
+import Questions from './Quest';
 
 export default function App() {
   return (
     <div>
-      <File />
+      <Questions />
     </div>
   );
 }
